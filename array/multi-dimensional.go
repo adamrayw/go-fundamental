6cd9738c1514642ff36cd169c0da2f386a997057
adamrayw <raywibowo68@gmail.com>
2023-02-05 02:34:51 +0700

    Array Multi Dimensional

---
+++ array/multi-dimensional.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	multiDimen := [3][3]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+
+	for i := 0; i < 3; i++ {
+		for x := 0; x < 3; x++ {
+			fmt.Println(multiDimen[i][x], [])
+		}
+	}
+}
