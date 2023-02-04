2cdb7e50bf55574b3f7ba810a234b03031d4340b
adamrayw <raywibowo68@gmail.com>
2023-02-05 01:30:25 +0700

    array | 1-array | done!

--- assignment/gopher-basic-starter/sesi-1/array/1-array-basic/main.go
+++ assignment/gopher-basic-starter/sesi-1/array/1-array-basic/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Array adalah suatu tipe data yang dapat menampung suatu variable dengan tipe yang sama, dan ukuran array sudah ditentukan diawal
 
 func main() {
@@ -12,6 +14,11 @@ func main() {
 
 	//TODO: Buatlah array of int dengan ukuran 10 lalu cetak isi nya dengan perintah fmt.println()
 	//start_answer
+	var array1 = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for i := 0; i < len(array1); i++ {
+		fmt.Println(array1[i])
+	}
 
 	//begin_answer
 }
