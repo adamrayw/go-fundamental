77aca91c2858ef1ae2635f17ce96950ccdebbdae
adamrayw <raywibowo68@gmail.com>
2023-02-05 20:03:40 +0700

    array | 3-write-array | done!

--- assignment/gopher-basic-starter/sesi-1/array/3-write-array/main.go
+++ assignment/gopher-basic-starter/sesi-1/array/3-write-array/main.go
@@ -13,7 +13,11 @@ func main() {
 	//TODO: isi array namaKota dengan nama kota
 	// urutan nama kota: Jakarta, Bandung, Surabaya, Semarang, dan Makassar
 	//start_answer
-
+	namaKota[0] = "Jakarta"
+	namaKota[1] = "Bandung"
+	namaKota[2] = "Surabaya"
+	namaKota[3] = "Semarang"
+	namaKota[4] = "Makassar"
 	//end_answer
 
 	fmt.Println(namaKota) // [Jakarta Bandung Surabaya Medan Makassar]
