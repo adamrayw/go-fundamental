60984557bdc112a4964c215a38b758acdc6f26dd
adamrayw <raywibowo68@gmail.com>
2023-02-06 08:02:32 +0700

    slice | 1-slice-basic | done!

--- assignment/gopher-basic-starter/sesi-1/slice/1-slice-basic/main.go
+++ assignment/gopher-basic-starter/sesi-1/slice/1-slice-basic/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 Kalian sudah tau tentang array kan, sekarang kita akan mencoba slice
 Slice bisa dibilang mirip dengan array, perbedaannya adalah ukuran slice fleksibel, dan tidak butuh ditetapkan ukurannya
@@ -14,16 +16,26 @@ func main() {
 	*/
 	//TODO: Buatlah sebuah slice of int dan isi d
 	//start_answer
-
+	sliceInt := []int{}
 	//end_answer
 
 	//TODO: isikan int sebanyak 10 buah pada slice yang dibuat
 	//HINT: gunakan append
 	//start_answer
+	sliceInt = append(sliceInt, 1)
+	sliceInt = append(sliceInt, 2)
+	sliceInt = append(sliceInt, 3)
+	sliceInt = append(sliceInt, 4)
+	sliceInt = append(sliceInt, 5)
+	sliceInt = append(sliceInt, 6)
+	sliceInt = append(sliceInt, 7)
+	sliceInt = append(sliceInt, 8)
+	sliceInt = append(sliceInt, 9)
+	sliceInt = append(sliceInt, 10)
 	//end_answer
 
 	//TODO: cetak seluruh nilai slice menggunakan fmt.println
 	//start_answer
-
+	fmt.Println(sliceInt)
 	//end_answer
 }
