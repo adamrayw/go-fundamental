c3e56761bcd3ffdb994c217f33dbcf5d1c58f7f9
adamrayw <raywibowo68@gmail.com>
2023-02-07 09:06:47 +0700

    assignment | function | 1-function-basic | done!

--- assignment/gopher-basic-starter/sesi-2/function/1-function-basic/main.go
+++ assignment/gopher-basic-starter/sesi-2/function/1-function-basic/main.go
@@ -5,21 +5,27 @@ import "fmt"
 //Fungsi HelloWorld tanpa parameter dan tanpa return value
 //TODO: Buatlah fungsi untuk mencetak "Hello World"
 //start_answer
-
+func HelloWorld() {
+	fmt.Println("Hello World")
+}
 
 //end_answer
 
 //Fungsi HelloName dengan single parameter tanpa return value
 //TODO: Buatlah fungsi yang menerima nama(string) sebagai input dan cetak "Hello Nama", eg input:rinal output:Hello rinal
 //start_answer
-
+func HelloName(nama string) {
+	fmt.Println(nama)
+}
 
 //end_answer
 
 //Fungsi Add dengan multiple parameter dengan return value
 //TODO: Buatlah fungsi yang menerima dua buah bilangan, dan balikan nilai perkaliannya
 //start_answer
-
+func Add(a int, b int) int {
+	return a * b
+}
 
 //end_answer
 
