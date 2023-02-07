4516e1c81fd5eead9bb850b13cbf0ee2f6fd7e83
adamrayw <raywibowo68@gmail.com>
2023-02-07 08:27:52 +0700

    sesi-1 | final-project | store-app | done!

--- assignment/gopher-basic-starter/sesi-1/store-app/main.go
+++ assignment/gopher-basic-starter/sesi-1/store-app/main.go
@@ -15,13 +15,18 @@ func main() {
 		durian:50000
 	*/
 	//start_answer
-	
+	mapProduct := make(map[string]int)
+
+	mapProduct["pisang"] = 10000
+	mapProduct["jeruk"] = 5000
+	mapProduct["semangka"] = 20000
+	mapProduct["durian"] = 50000
 	//end_answer
 	fmt.Println(mapProduct)
 
 	//TODO: Buatlah slice of int, yang akan digunakan untuk menampung harga product yang dipilih, nama slice "slicePrices" (2)
 	//start_answer
-	
+	slicePrices := []int{}
 	//end_answer
 
 	// Sekarang kita akan membuat seolah-oleh pembeli membeli pisang, jeruk, dan semangka
@@ -29,7 +34,9 @@ func main() {
 	//HINT: gunakan append pada slice dan nilai nya diambil dari map
 	//TODO: tambahkan harga pisang, jeruk, dan semangka ke slice
 	//start_answer
-	
+	slicePrices = append(slicePrices, mapProduct["pisang"])
+	slicePrices = append(slicePrices, mapProduct["jeruk"])
+	slicePrices = append(slicePrices, mapProduct["semangka"])
 	//end_answer
 	fmt.Println(slicePrices)
 
@@ -37,12 +44,19 @@ func main() {
 	//TODO: hitung total harga di slicePrices, dan taruh total nya ke variable totalPrice (3)
 	//HINT: gunakan for range pada slice, dan jumlahkan nilainya
 	//start_answer
-	
+
+	for _, value := range slicePrices {
+		totalPrice += value
+	}
 	//end_answer
 
 	//TODO: jika harga lebih besar dari 10000 maka pelanggan akan mendapatkan diskon 1000, lalu cetak total harga nya,
 	// jika tidak maka cetak total harga saja
 	//start_answer
-	
+	if totalPrice > 10000 {
+		finalPrice := totalPrice - 1000
+		fmt.Println("Selamat kamu mendapatkan diskon 1000!")
+		fmt.Println("Total :", finalPrice)
+	}
 	//end_answer
 }
