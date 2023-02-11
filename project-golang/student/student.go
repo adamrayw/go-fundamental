fc89d063dc9ba27510fe76dceab9eae51395d694
adamrayw <raywibowo68@gmail.com>
2023-02-11 23:51:36 +0700

    Package student

---
+++ project-golang/student/student.go
@@ -0,0 +1,29 @@
+package student
+
+type Student struct {
+	Name  string
+	Age   int
+	Score int
+}
+
+type StudentInterface interface {
+	GetName() string
+	GetAge() int
+	ScoreConvert() int
+}
+
+func (s *Student) GetName() string {
+	return s.Name
+}
+
+func (s *Student) GetAge() int {
+	return s.Age
+}
+
+func (s *Student) getScore() string {
+	if s.Score > 80 {
+		return "Awesome!"
+	} else {
+		return "Good"
+	}
+}
